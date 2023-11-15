e85fd02bca5db2898a5ad7766d920d0b43896ff3
Brariv <119123309+Brariv@users.noreply.github.com>
2023-11-14 19:15:52 -0600

    Update driver.java

--- Controller/driver.java
+++ Controller/driver.java
@@ -1,5 +1,12 @@
 package Controller;
 
+import View.Interface;
+import View.MenuUi;
+
 public class driver {
-    
+    public static void main(String[] args) {
+        Interface interface1 = new MenuUi();
+
+        interface1.menu();
+    }
 }
